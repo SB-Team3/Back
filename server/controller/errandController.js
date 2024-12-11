6041cc2dbcd5868b0e4ed8ab606cf118b4ef86ea
juhokim <billbill415415415@gmail.com>
2024-12-11 15:52:47 +0900

    hello

--- server/controller/errandController.js
+++ server/controller/errandController.js
@@ -2,10 +2,10 @@ import * as errandQuery from '../query/errandQuery.js';
 
 export async function creErrand(req, res, next) {
     try {
-        const errand = await errandQuery.create(req.body);
-        res.status(201).json(errand);
+        const errand = await errandQuery.create(req.body)
+        res.status(201).json(errand)
     } catch (error) {
-        console.error(error);
-        res.status(400).json({ message: '등록이 불가합니다', error: error.message });
+        console.error(error)
+        res.status(400).json({ message: '등록이 불가합니다', error: error.message })
     }
 }
